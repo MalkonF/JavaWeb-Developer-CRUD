71274f7e7c0db63ed1a7ec816765af11483bce6c
Malkon Faria <sudo777@protonmail.com>
2020-12-12 00:41:35 -0300

    First commit

---
+++ src/main/java/com/malkon/JavaWebDeveloperCRUD/JavaWebDeveloperCrudApplication.java
@@ -0,0 +1,13 @@
+package com.malkon.JavaWebDeveloperCRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebDeveloperCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWebDeveloperCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/malkon/JavaWebDeveloperCRUD/JavaWebDeveloperCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malkon.JavaWebDeveloperCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWebDeveloperCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
