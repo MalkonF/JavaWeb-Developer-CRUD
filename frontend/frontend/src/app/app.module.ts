cca2da2f5a583b72995825b1f29d16ed5f969275
Malkon Faria <sudo777@protonmail.com>
2020-12-16 21:52:08 -0300

    Creating components for product

--- frontend/frontend/src/app/app.module.ts
+++ frontend/frontend/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ProductNewComponent } from './product-new/product-new.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    ProductDetailsComponent,
+    ProductNewComponent,
+    ProductEditComponent
   ],
   imports: [
     BrowserModule
