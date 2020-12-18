af199d4a8d9f37fa616045b0af9afaae50ae8469
Malkon Faria <sudo777@protonmail.com>
2020-12-18 20:31:52 -0300

    Department scheduling implemented

---
+++ src/main/java/com/malkon/JavaWebDeveloperCRUD/services/utils/DepartmentSchedule.java
@@ -0,0 +1,26 @@
+package com.malkon.JavaWebDeveloperCRUD.services.utils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import com.malkon.JavaWebDeveloperCRUD.domain.Department;
+import com.malkon.JavaWebDeveloperCRUD.services.DepartmentService;
+
+@Component
+public class DepartmentSchedule {
+	
+	@Autowired
+	DepartmentService departmentService;
+
+	private final long SECONDS = 15;
+	
+	@Scheduled(fixedDelay = SECONDS) 
+    public void verifyDepartmentPerHour() { 
+        RestTemplate restTemplate = new RestTemplate();
+        Department department = restTemplate.getForObject("https://private-anon-0b6825f1d3-maximatech.apiary-mock.com/fullstack/departamento", Department.class);
+        departmentService.update(department);
+	}
+
+}
